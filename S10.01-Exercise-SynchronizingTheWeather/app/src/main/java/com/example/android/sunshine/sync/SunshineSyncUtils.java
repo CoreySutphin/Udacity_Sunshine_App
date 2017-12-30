a55c6d0b71b0a0915b0e3e6507f2ff5362a4191c
Corey Sutphin <sutphincj@vcu.edu>
2017-12-30 00:50:53 +0000

    Creating a sync task to update weather information

---
+++ S10.01-Exercise-SynchronizingTheWeather/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -0,0 +1,15 @@
+package com.example.android.sunshine.sync;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+// COMPLETED (9) Create a class called SunshineSyncUtils
+    //  COMPLETED (10) Create a public static void method called startImmediateSync
+    //  COMPLETED (11) Within that method, start the SunshineSyncIntentService
+public class SunshineSyncUtils {
+    public static void startImmediateSync(@NonNull final Context context) {
+        Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
+        context.startService(intentToSyncImmediately);
+    }
+}
